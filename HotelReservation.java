0e5b925848067ef5cfc8679be1b6fb65f710b300
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-12 04:24:13 +0530

    [Surajmal Kumar].initial commit.

---
+++ HotelReservation.java
@@ -0,0 +1,10 @@
+package hotelreservationsystem;
+
+
+public class HotelReservation 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Hotel Reservation System" );
+    }
+}

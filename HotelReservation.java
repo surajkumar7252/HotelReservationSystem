4a0c7b1677f5ea74517e184a35e95156608dd758
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-12 08:44:42 +0530

    [Surajmal Kumar].adding UC1.adding the functionality to add a hotel in a hotel reservation system

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,10 +1,45 @@
 package hotelreservationsystem;
 
 
-public class HotelReservation 
-{
-    public static void main( String[] args )
+import java.util.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+ class Hotels {
+	private String nameOfHotel;
+	private int dailyRent;
+	
+	public Hotels(String nameOfHotel, int dailyRent) {
+		
+		this.nameOfHotel = nameOfHotel;
+		this.dailyRent = dailyRent;
+	}
+}
+
+public class HotelReservation {
+	public static Scanner inputFeed=new Scanner(System.in);
+	public static final Logger logger=LogManager.getLogger(HotelReservation.class);
+	public static List<Hotels> listOfHotels=new ArrayList<>();
+   
+	public static void addingHotel() {
+		char choice=0;
+		while(choice=='Y'|| choice=='y') {
+			 logger.debug("Enter  name of the hotel: ");
+			String nameOfHotel=inputFeed.nextLine();
+			 logger.debug("Enter cost of staying in hotel: ");
+			int dailyRent=inputFeed.nextInt();
+			Hotels hotelOb=new Hotels(nameOfHotel,dailyRent);
+			listOfHotels.add(hotelOb);
+			 logger.debug("Do you want to perform again: ");
+			choice=inputFeed.next().charAt(0);
+			
+		}
+	}
+	
+	public static void main( String[] args )
     {
-        System.out.println( "Welcome to Hotel Reservation System" );
+       logger.debug( "Welcome to Hotel Reservation System" );
+       addingHotel();
     }
 }

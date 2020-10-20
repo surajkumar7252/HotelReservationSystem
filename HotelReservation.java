0ec92281031612ecbdff3f1a5b92d143a755d159
surajkumar7252 <71683354+surajkumar7252@users.noreply.github.com>
2020-10-20 11:09:49 +0530

    modifying some Identifiers to accommodate future changes.

--- HotelReservation.java
+++ HotelReservation.java
@@ -75,7 +75,7 @@ public class HotelReservation {
 			LocalDate checkOutFeed = LocalDate.parse(checkOutDate, dateFormat);
 			Integer numOfDays = (Integer) ChronoUnit.DAYS.between(checkInDate,checkOutDate) + 1;
 			Map<Hotels, Integer> hotelRateMap=listOfHotels.stream().collect(Collectors.toMap(hotelOb->hotelOb, hotelOb->hotelOb.getWeekDayRate()*numOfDays));
-			Hotels cheapestHotelInAll=hotelRateMap.keySet().stream().min((n1,n2)->hotelRateMap.get(n1)-hotelRateMap.get(n2)).orElse(null);
+			Hotels cheapestHotelInAll=hotelRateMap.keySet().stream().min((firstHotel,secondHotel)->hotelRateMap.get(firstHotel)-hotelRateMap.get(secondHotel)).orElse(null);
 			log.info(cheapestHotelInAll.getName()+",Rate(in Dollars) : "+hotelRateMap.get(cheapestHotelInAll));
 			log.info("The cheapest Hotel is :"+ cheapestHotelInAll);
 		}

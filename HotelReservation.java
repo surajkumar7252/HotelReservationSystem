e487399b54281481b4c6ea1e0f464d7ea0311166
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-20 10:38:24 +0530

    [Surajmal Kumar].Adding UC2.Adding ability to find cheapest hotel in a given date range.

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,7 +1,11 @@
 package hotelreservationsystem;
 
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,31 +19,74 @@ import org.apache.logging.log4j.Logger;
 		this.nameOfHotel = nameOfHotel;
 		this.dailyRent = dailyRent;
 	}
+	
+	public String getName() {
+		return nameOfHotel;
+	}
+	public void setName(String name) {
+		this.nameOfHotel = name;
+	}
+	public int getRate() {
+		return dailyRent;
+	}
+	public void setRate(int rate) {
+		this.dailyRent = rate;
+	}
 }
 
 public class HotelReservation {
 	public static Scanner inputFeed=new Scanner(System.in);
-	public static final Logger logger=LogManager.getLogger(HotelReservation.class);
+	public static final Logger log=LogManager.getLogger(HotelReservation.class);
 	public static List<Hotels> listOfHotels=new ArrayList<>();
    
 	public static void addingHotel() {
 		char choice=0;
 		while(choice=='Y'|| choice=='y') {
-			 logger.debug("Enter  name of the hotel: ");
+			log.info("Enter  name of the hotel: ");
 			String nameOfHotel=inputFeed.nextLine();
-			 logger.debug("Enter cost of staying in hotel: ");
+			log.info("Enter cost of staying in hotel: ");
 			int dailyRent=inputFeed.nextInt();
 			Hotels hotelOb=new Hotels(nameOfHotel,dailyRent);
 			listOfHotels.add(hotelOb);
-			 logger.debug("Do you want to perform again: ");
+			log.info("Do you want to perform again: ");
 			choice=inputFeed.next().charAt(0);
 			
 		}
 	}
+		
+		public static void cheapestHotel() {
+			 log.info("Enter  the check-in date in the format DDMMMYYYY (like 10OCT2020): ");
+		    String checkInDate=inputFeed.nextLine();
+		    log.info("Enter  the check-out date in the format DDMMMYYYY (like 12OCT2020): ");
+		    String checkOutDate=inputFeed.nextLine();
+			DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd MMM yyyy");
+			LocalDate checkInFeed= LocalDate.parse(checkInDate, dateFormat);
+			LocalDate checkOutFeed = LocalDate.parse(checkOutDate, dateFormat);
+			Integer numOfDays = (Integer) ChronoUnit.DAYS.between(checkInDate,checkOutDate) + 1;
+			Map<Hotels, Integer> hotelRateMap=listOfHotels.stream().collect(Collectors.toMap(hotelOb->hotelOb, hotelOb->hotelOb.getRate()*numOfDays));
+			Hotels cheapestHotelInAll=hotelRateMap.keySet().stream().min((n1,n2)->hotelRateMap.get(n1)-hotelRateMap.get(n2)).orElse(null);
+			log.info(cheapestHotelInAll.getName()+",Rate(in Dollars) : "+hotelRateMap.get(cheapestHotelInAll));
+			log.info("The cheapest Hotel is :"+ cheapestHotelInAll);
+		}
 	
-	public static void main( String[] args )
-    {
-       logger.debug( "Welcome to Hotel Reservation System" );
-       addingHotel();
-    }
-}
+	
+	public static void main( String[] args ){
+        Integer choice=0;
+       HotelReservation reservation=new HotelReservation ();
+       log.info( "Welcome to Hotel Reservation System" );
+       log.info("What operation do you want to perform: ");
+       log.info("1.Adding Hotels Data. ");
+       log.info("2.Finding the cheapest hotel in-between Dates. ");
+       
+	  choice=inputFeed.nextInt();
+	  switch(choice) {
+	  case 1:reservation.addingHotel();
+	          break;
+	  case 2:  log.info("The cheapest Hotel is :");
+	          
+	           reservation.cheapestHotel();
+	           break;
+	  }
+       
+        }
+   }
